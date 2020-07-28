99c8f058f3d8fdefa8b5d631567bc843397e1d24
DavidManheim <davidmanheim@gmail.com>
2020-07-28 09:54:37 +0300

    Change original China study summary script to allow for arbitrary percentile outputs

--- China IFR Model/source/mcmc_summary.R
+++ China IFR Model/source/mcmc_summary.R
@@ -1,6 +1,9 @@
 # Summarise a single column of mcmc output
 summarise_mcmc_single <- function(x, q = c(0.025, 0.975)){
   
+  # create output dataframe to populate:
+  output <- c(mode=numeric(), mean=numeric(), median=numeric()) 
+    
   # compute kernel density and normalise
   d <- density(x)
   d$y <- d$y/sum(d$y)
@@ -9,16 +12,17 @@ summarise_mcmc_single <- function(x, q = c(0.025, 0.975)){
   x_mode <- d$x[which.max(d$y)]
   x_mean = mean(x)
   x_median = median(x)
-  x_lower = quantile(x, q[1], names = FALSE)
-  x_upper = quantile(x, q[2], names = FALSE)
   
-  return(c(mode = x_mode, mean = x_mean, median = x_median, x_0.025 = x_lower, x_0.975 = x_upper))
-  
+  return(c(mode = x_mode, mean = x_mean, median = x_median, quantile(x, q))) #Modified to allow more qunatile outputs.
 }
+
 # Summarise multiple columns of MCMC output
 summarise_mcmc <- function(x, q = c(0.025, 0.975)){
   if(is.vector(x)){
     x <- matrix(x)
   }
-  data.frame(t(apply(x, 2,  summarise_mcmc_single, q = q)))
+  names = names(summarise_mcmc_single(x[,1], q=q))
+  out_df = data.frame(t(apply(x, 2,  summarise_mcmc_single, q = q)))
+  names(out_df) <- names
+  return(out_df)
 }
